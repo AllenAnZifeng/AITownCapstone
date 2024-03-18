9965889f615bba19771c8a5c297fefdc45d2be68
Angus Wang <wangjadehao@gmail.com>
2024-03-18 15:59:47 -0700

    added information transfer and decision making use cases (rebased)

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import argparse
 from models import Chat
 
-chat = Chat()
-chat.start()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="specify simulation case")
+    parser.add_argument("sim_case", type=str, help="simulation case to run. Can be:\n   1. information_transfer\n   2. problem_solving\n    3. decision_making")
+
+    args = parser.parse_args()
+
+    sim_case_options = ["information_transfer", "problem_solving", "decision_making"]
+    if args.sim_case not in sim_case_options:
+        parser.error('invalid simulation case')
+
+    chat = Chat(str(args.sim_case))
+    chat.start()
