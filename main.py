894d269801cefe98f2420ccd5891d7cef20e738a
Angus Wang <wangjadehao@gmail.com>
2024-03-18 17:11:34 -0700

    added evaluation prompts

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from models import Chat
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="specify simulation case")
     parser.add_argument("sim_case", type=str, help="simulation case to run. Can be:\n   1. information_transfer\n   2. problem_solving\n    3. decision_making")
+    parser.add_argument('-e', '--eval', action='store_true', help='Inlcude labels')
 
     args = parser.parse_args()
 
     sim_case_options = ["information_transfer", "problem_solving", "decision_making"]
     if args.sim_case not in sim_case_options:
         parser.error('invalid simulation case')
+    do_eval = bool(args.eval)
 
     chat = Chat(str(args.sim_case))
     chat.start()
+    if do_eval:
+        chat.eval_agents()
--- models.py
+++ models.py
@@ -25,27 +25,35 @@ class Agent:
 class Chat:
     def __init__(self, sim_case: str):
         with open('prompts.json', 'r', encoding='utf-8') as f:
-            prompt_data = json.load(f)
+            data = json.load(f)
 
-        self.max_turns = prompt_data["max_turns"]
-        self.system_prompt = prompt_data['system_prompt']
-        self.user_prompt = prompt_data['user_prompt']
-        self.topic = prompt_data[sim_case]["topic"]
-        self.response_format = prompt_data[sim_case]["response_format"]
-        self.num_agents = prompt_data[sim_case]["num_agents"]
+        convo_data = data["conversation"]
+        self.convo_max_turns = convo_data["max_turns"]
+        self.convo_system_prompt = convo_data['system_prompt']
+        self.convo_user_prompt = convo_data['user_prompt']
+
+        eval_data = data["evaluation"]
+        self.eval_system_prompt = eval_data['system_prompt']
+        self.eval_user_prompt = eval_data['user_prompt']
+
+        sim_case_data = data["simulation_cases"][sim_case]
+        self.topic = sim_case_data["topic"]
+        self.response_format = sim_case_data["response_format"]
+        self.evaluation_prompts = sim_case_data["evaluation_prompts"]
+        self.num_agents = sim_case_data["num_agents"]
         self.agents = []
-        for agent in prompt_data[sim_case]["agents"]:
+        for agent in sim_case_data["agents"]:
             new_agent = Agent(agent["id"], agent["background"])
             self.agents.append(new_agent)
-        
+                    
         with open('config.secret', 'r') as f:
             api_key = f.read()
         self.client = OpenAI(api_key=api_key)
 
 
     def getResponse(self, order: int) -> str:
-        complete_system_prompt = self.system_prompt.format(self.num_agents, self.topic, self.response_format)
-        complete_user_prompt = self.user_prompt.format(order, self.agents[order - 1].getBackground(), self.agents[order - 1].getConvoMemory(), order)
+        complete_system_prompt = self.convo_system_prompt.format(self.num_agents, self.topic, self.response_format)
+        complete_user_prompt = self.convo_user_prompt.format(order, self.agents[order - 1].getBackground(), self.agents[order - 1].getConvoMemory(), order)
         # print()
         # print()
         # print("===============================================")
@@ -74,13 +82,13 @@ class Chat:
         return content
 
     def getOrder(self):  # round robin
-        return [i + 1 for i in range(self.num_agents)] * ((self.max_turns // self.num_agents) + 1)
+        return [i + 1 for i in range(self.num_agents)] * ((self.convo_max_turns // self.num_agents) + 1)
 
     def start(self):
         count = 0
         orders = self.getOrder()
 
-        while count < self.max_turns:
+        while count < self.convo_max_turns:
             response = self.getResponse(orders[count]).strip()
             count += 1
             print(response)
@@ -90,6 +98,33 @@ class Chat:
                 break
 
         print('Chat ended')
+    
+    def eval_agents(self):
+        for i, agent in enumerate(self.agents):
+            print("===============================================")
+            print(f"evaluating agent {i + 1}:")
+            complete_system_prompt = ""
+            complete_user_prompt = ""
+            for prompt in self.evaluation_prompts:
+                complete_system_prompt = self.eval_system_prompt.format(prompt)
+                complete_user_prompt = self.eval_user_prompt.format(i + 1, agent.getBackground(), agent.getConvoMemory(), i + 1)
+                print(f"evaluation prompt: {prompt}")
+                response = self.client.chat.completions.create(
+                    model="gpt-4",
+                    messages=[
+                        {
+                            "role": "system",
+                            "content": complete_system_prompt
+                        },
+                        {
+                            "role": "user",
+                            "content": complete_user_prompt
+                        }
+                    ]
+                )
+
+                content = response.choices[0].message.content
+                print(f"answer: {content}")
 
 
 if __name__ == '__main__':
